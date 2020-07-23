21b44ed366a43b1ae170199cda1185b9990ca6fa
DolngNothing <1178363215@qq.com>
2020-07-23 11:51:10 +0800

    [christopher] feature: add should_return_1_when_play_fizz_buzz_whizz_given_1 test

---
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -0,0 +1,10 @@
+package junit5.tdd;
+
+public class FizzBuzzWhizz {
+
+
+    public static void main(String[] args) {
+        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
+        fizzBuzzWhizz.play(1);
+    }
+}
---
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -0,0 +1,19 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzWhizzTest {
+    @Test
+    void should_return_1_when_play_fizz_buzz_whizz_given_1() {
+        //given
+        int number = 1;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        //when
+        String actual = fizzBuzzWhizz.play(number);
+
+        //should
+        assertEquals("1", actual);
+    }
+}
