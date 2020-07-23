cca75d4a2daf06b16bfd729a0315288ce38c5ee5
DolngNothing <1178363215@qq.com>
2020-07-23 12:00:06 +0800

    [christopher] feature: give 3 when play fizz buzz whizz return fizz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -3,6 +3,10 @@ package junit5.tdd;
 public class FizzBuzzWhizz {
     public String play(int number) {
         StringBuffer result = new StringBuffer();
+        if(number % 3 ==0){
+            result.append("fizz");
+        }
+
         if (result.length() == 0) {
             result.append(number);
         }
