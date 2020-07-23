d1937ed5ebf0b462678a124caa5a97fdf1b2e7e5
DolngNothing <1178363215@qq.com>
2020-07-23 12:38:58 +0800

    [christopher] feature: give 15 when play fizz buzz whizz should return fizzbuzz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -3,15 +3,15 @@ package junit5.tdd;
 public class FizzBuzzWhizz {
     public String play(int number) {
         StringBuffer result = new StringBuffer();
-        if(number % 3 ==0){
+        if (number % 3 == 0) {
             result.append("fizz");
         }
 
-        if(number % 5==0){
+        if (number % 5 == 0) {
             result.append("buzz");
         }
 
-        if(number % 7==0){
+        if (number % 7 == 0) {
             result.append("whizz");
         }
 
